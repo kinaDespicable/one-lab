ef43cf29386693e423a668c4c7944dea5fa617c0
Daniyar Kalkamanov <danik.kalkamano@gmail.com>
2023-07-17 00:30:58 +0600

    [QUICK FIX] Formatting changes

--- src/main/java/one/lab/firstpractice/mapper/NewsMapper.java
+++ src/main/java/one/lab/firstpractice/mapper/NewsMapper.java
@@ -2,38 +2,34 @@ package one.lab.firstpractice.mapper;
 
 import lombok.RequiredArgsConstructor;
 import one.lab.firstpractice.model.entity.News;
-import one.lab.firstpractice.model.entity.Topic;
 import one.lab.firstpractice.model.entity.User;
-import one.lab.firstpractice.service.TopicService;
 import one.lab.firstpractice.service.UserService;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 
 @Component
 @RequiredArgsConstructor
 public class NewsMapper implements RowMapper<News> {
 
     private final UserService userService;
-    private final TopicService topicService;
 
     @Override
     public News mapRow(ResultSet rs, int rowNum) throws SQLException {
 
         User author = userService.fetchById(rs.getLong("author_id"));
-        Topic topic = topicService.fetchById(rs.getLong("topic_id"));
+        Timestamp timestamp = (Timestamp) rs.getObject("published_at");
 
         return News.builder()
                 .id(rs.getLong("news_id"))
                 .title(rs.getString("title"))
                 .content(rs.getString("content"))
                 .likes(rs.getLong("likes"))
-                .publishedAt((LocalDateTime) rs.getObject("published_at"))
+                .publishedAt(timestamp.toLocalDateTime())
                 .author(author)
-                .topic(topic)
                 .build();
     }
 
--- src/main/java/one/lab/firstpractice/mapper/UserMapper.java
+++ src/main/java/one/lab/firstpractice/mapper/UserMapper.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Component
-public class UserMapper implements RowMapper<User>{
+public class UserMapper implements RowMapper<User> {
 
     @Override
     public User mapRow(ResultSet resultSet, int rowNum) throws SQLException {
