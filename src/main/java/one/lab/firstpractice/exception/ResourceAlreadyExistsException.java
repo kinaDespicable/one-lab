0c7da3e4652778d84c68ae565cbb87fc473f28ec
Daniyar Kalkamanov <danik.kalkamano@gmail.com>
2023-07-21 10:43:38 +0600

    Exception package was removed

--- src/main/java/one/lab/firstpractice/exception/ResourceAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package one.lab.firstpractice.exception;
-
-public class ResourceAlreadyExistsException extends RuntimeException{
-
-    public ResourceAlreadyExistsException(String message) {
-        super(message);
-    }
-}
